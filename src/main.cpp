808937e2cbbc7914f6f6bb76db040788a9eb0f2e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-08-29 20:54:13 +0700

    Add SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
-#include <pspdebug.h>
-#include <pspdisplay.h>
-#include <pspkernel.h>
+#include <GLES/gl.h>
+#include <SDL.h>
+#include <stdexcept>
 
-PSP_MODULE_INFO("Cobblestone", 0, 1, 0);
-PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
-
-int main() {
-  pspDebugScreenInit();
-  while (1) {
-    pspDebugScreenSetXY(0, 0);
-    pspDebugScreenPrintf("Hello, world!");
-    sceDisplayWaitVblank();
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
+    throw std::runtime_error("Failed to initialize SDL");
   }
+
+  SDL_Window *window =
+      SDL_CreateWindow("Cobblestone", SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, 480, 272, SDL_WINDOW_OPENGL);
+  if (!window) {
+    throw std::runtime_error("Failed to create SDL window");
+  }
+
+  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+  if (!gl_context) {
+    throw std::runtime_error("Failed to create OpenGL context");
+  }
+
+  bool running = true;
+  SDL_Event event;
+  while (running) {
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+
+    glClearColor(1.0, 1.0, 0.0, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(gl_context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
   return 0;
 }
