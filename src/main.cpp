96695ba784d0fb2799bb5ec30ad525c85910936f
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-09-24 22:48:25 +0700

    Fix linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#include <GL/gl.h>
-#include <SDL.h>
-#include <iostream>
-#include <stdexcept>
-
 #ifdef __PSP__
 #include <exception>
 #include <pspdebug.h>
@@ -33,45 +28,6 @@ int main(int argc, char *argv[]) {
 #ifdef __PSP__
   std::set_terminate(terminate);
 #endif
-
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
-    throw std::runtime_error("Failed to initialize SDL");
-  }
-
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-
-  SDL_Window *window =
-      SDL_CreateWindow("Cobblestone", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 480, 272, SDL_WINDOW_OPENGL);
-  if (!window) {
-    throw std::runtime_error("Failed to create SDL window");
-  }
-
-  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-  if (!gl_context) {
-    throw std::runtime_error("Failed to create OpenGL context");
-  }
-
-  bool running = true;
-  SDL_Event event;
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        running = false;
-      }
-    }
-
-    glClearColor(1, 0, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    SDL_GL_SwapWindow(window);
-  }
-
-  SDL_GL_DeleteContext(gl_context);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
 
   return 0;
 }
